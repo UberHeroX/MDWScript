cc36778e8ec350a5ee844305b6f5cfa6402c228f
UberHeroX <96598023+UberHeroX@users.noreply.github.com>
2024-01-18 05:46:50 +0100

    Add files via upload

---
+++ ReadMe.txt.txt
@@ -0,0 +1,15 @@
+Poz,
+
+U IzakBotScript.py, ti se nalazi sve od informacija koje treba da korigujes po zelji da bi namestio vreme, 
+broj dana, broj sobe i ostalo kako god ti je potrebno. 
+
+U main.py scripti ti se nalazi podesavanje za tajmer, tj za vreme aktivacije. Programu treba koja sekunda da se upali tako da 
+imaj to na umu. Jako je bitno takodje da skines ChromeEngine, link za to sam ostavio u komentaru u IzakBotScript.
+Pazi da treba dobro da postavis lokaciju gde ti se nalazi Engine, u suprotnom ti nece raditi.
+
+Mora da se koristi GoogleChrome i mora da bude instaliran. Ostale stvari ako hoces da instaliras samo jednom u prazno upali 
+IzakBotScript i skinuce ti sve potrebne komponente. Napravio sam to tako da ne bi morao da ucis kako se instaliraju.
+
+Proveri sam par puta da l radi skripta, pre nego sto je ustvari primenis u praksi. 
+
+Laku noc.
---
+++ main.py
@@ -0,0 +1,65 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+import datetime
+import time
+import os
+import sys
+import threading
+
+def SetupScript():
+    print("Skripta aktivirana...")
+    import IzakBotScript
+
+def clear_screen():
+    # Windows
+    if sys.platform == "win32":
+        os.system('cls')
+   
+def CustomTimer():
+ while True:
+    current_time = datetime.datetime.now()
+    time_left = target_time - current_time
+    if time_left.total_seconds() > 0:
+        clear_screen() 
+        time_left_str = str(time_left).split('.')[0]
+        print(f"Vreme do aktivacije: {time_left_str}")
+        time.sleep(1)  # Update the countdown every second
+    else:
+        break
+
+
+scheduler = BlockingScheduler()
+
+
+#Vreme za aktivaciju. 
+#Godina|Mesec|Dan|Sat|Minut|Sekund
+
+Godina= 2024
+Mesec= 1
+Dan= 18
+Sat= 5
+Minut= 39
+Sekunda= 5
+
+now = datetime.datetime.now()
+target_time = now.replace(hour=Sat, minute=Minut, second=Sekunda)
+
+scheduler.add_job(SetupScript, 'date', run_date=datetime.datetime(Godina, Mesec, Dan, Sat, Minut, Sekunda))
+print("Tajmer uspesno postavljen")
+
+countdown_thread = threading.Thread(target=CustomTimer)
+countdown_thread.start()
+
+
+scheduler.start()
+
+
+
+
+
+
+
+
+
+
+
